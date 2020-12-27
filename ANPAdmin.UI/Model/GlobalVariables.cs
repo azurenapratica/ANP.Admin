281ad5bdfa5a890647c3641b4706a9e6d13d0ab0
Milton Câmara <miltoncamaragomes@gmail.com>
2020-12-27 17:47:16 -0300

    Regra para guardar a região no cache da aplicação

---
+++ ANPAdmin.UI/Model/GlobalVariables.cs
@@ -0,0 +1,13 @@
+﻿namespace ANPAdmin.UI.Model
+{
+    public static class GlobalVariables
+    {
+        static string _ActualRegion;
+
+        public static string ActualRegion
+        {
+            get { return _ActualRegion; }
+            set { _ActualRegion = value; }
+        }
+    }
+}
