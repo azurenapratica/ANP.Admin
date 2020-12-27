65a2a3c9031dda4a8a25ee29bda0988c9f8ca266
Milton Câmara <miltoncamaragomes@gmail.com>
2020-12-27 16:01:28 -0300

    Adicionado suporte a App Insights

--- ANPAdmin.UI/Startup.cs
+++ ANPAdmin.UI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.ApplicationInsights.DependencyCollector;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,13 @@ namespace ANPAdmin.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationInsightsTelemetry(Configuration);
+            services.ConfigureTelemetryModule<DependencyTrackingTelemetryModule>(
+                (module, o) =>
+                {
+                    module.EnableSqlCommandTextInstrumentation = true;
+                });
+
             services.AddRazorPages();
         }
 
